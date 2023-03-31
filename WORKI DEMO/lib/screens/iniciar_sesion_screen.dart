fe4bd09c147d35702647c17cd48976d8390faec7
David Leal <davinchi.leal123@gmail.com>
2023-03-30 23:40:49 -0500

    Logos en carpeta image

--- WORKI DEMO/lib/screens/iniciar_sesion_screen.dart
+++ WORKI DEMO/lib/screens/iniciar_sesion_screen.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
 
 class IniciarSesionScreen extends StatelessWidget {
-  
   const IniciarSesionScreen({Key? key}) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         title: const Text("Iniciar Sesion"),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Image.asset("images/logo.png",),
+          ],
+        ),
       ),
     );
   }
